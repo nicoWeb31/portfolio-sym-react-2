097eaef90c109b8fd02f362f9dc10b262cb83833
nicoWeb31 <nico.riot@free.fr>
2020-08-27 15:19:06 +0200

    mise en place de react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from "react-dom";
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,8 +9,14 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+console.log("test")
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+const App = ()=>{
+    return <h1>Hello world</h1>
+}
+
+const rootElement = document.querySelector("#app");
+ReactDOM.render(<App/>,rootElement)
